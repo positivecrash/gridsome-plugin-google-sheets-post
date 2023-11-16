d86129d0f5978630832d4c0efa199750fbd432ab
Julia <uug11@mail.ru>
2023-11-16 11:46:10 +0400

    new capthca

--- Form.vue
+++ Form.vue
@@ -1,25 +1,24 @@
 <template>
-  <form ref="form"  @submit.prevent="onSubmit" v-if="!isDisabled" :class="{'captcha-error': isError}">
+  <form ref="form"  @submit.prevent="onSubmit" :class="{'captcha-error': isError}">
 
-    <input @input="checkCaptcha" :model="captcha" v-if="!isSuccess && isCaptchaShown" class="jCaptcha" type="text" placeholder="Type in captcha result please">
-
-    <!-- honeypot field. This field should trigger captcha for suspicious (non-human) users -->
-    <label for="user-type">user tracker </label><input @input="checkUserType" id="user-type" name="user-type" size="40" tabindex="-1" type="text" :modal="userType" />
+      <vue-hcaptcha
+        ref="invisibleHcaptcha"
+        :sitekey="siteKey"
+        size="invisible"
+        theme="dark"
+        @verify="onVerify"
+        @error="onError"
+      />
 
     <slot/>
 
   </form>
 
-  <div class="noForm" v-else>
-    <p>Too many tries were executed.</p>
-    <p>If you are sure you are human, please, try again a bit later!</p>
-  </div>
-
 </template>
 
 <script>
 
-import jCaptcha from 'js-captcha';
+import VueHcaptcha from '@hcaptcha/vue-hcaptcha';
 
 export default {
 name: 'gspForm',
@@ -30,33 +29,22 @@ props: {
     required: true,
     default: null
   },
-  captchaStyle: {
-    type: Object,
-    required: false,
-    default: () => ({
-      width: 100,
-      height: 15,
-      textBaseline: 'top',
-      font: '15px Arial',
-      textAlign: 'left',
-      fillStyle: '#6E9BEF'
-    })
+  siteKey: {
+    type: String,
+    require: true
   }
+},
+
+components: {
+  VueHcaptcha
 },
 
 data() {
   return {
-    captcha: '',
-    userType: '',
-    time: null,
-    formElementsCount: null,
-    allInputs : [],
-    allTextareas: [],
     isError: false,
-    isSuccess: false,
-    isDisabled: false,
-    isSuspiciousUser: false,
-    isCaptchaShown: false,
+    verified: false,
+    token: null,
+    eKey: null,
   }
 },
 
@@ -68,75 +56,13 @@ metaInfo: {
   ]
 },
 
-computed: {
-  getCaptcha: function () {
-    return new jCaptcha({
-      el: '.jCaptcha',
-      canvasClass: 'jCaptchaCanvas',
-      canvasStyle: {
-          width: this.captchaStyle.width,
-          height: this.captchaStyle.height,
-          textBaseline: this.captchaStyle.textBaseline,
-          font: this.captchaStyle.font,
-          textAlign: this.captchaStyle.left,
-          fillStyle: this.captchaStyle.fillStyle
-      },
-      callback: ( response, $captchaInputElement, numberOfTries ) => {
-          if ( response == 'success' ) {
-              this.onVerify();
-              this.time = null;
-              this.formTime = null;
-          }
-          if ( response == 'error' ) {
-              this.onError();
-
-              if (numberOfTries === 3) {
-                  this.isDisabled = true
-
-                  setTimeout(() => {
-                    this.isError = false,
-                    this.isSuccess = false,
-                    this.isSuspiciousUser = false
-                    this.isDisabled = false
-                    this.time = null;
-                    this.formTime = null;
-                  }, 5000)
-              }
-          }
-      }
-    });
-  },
-
-  acceptableTime() {
-
-    if(this.formElementsCount === 1) {
-      return 450
-    }
-
-    if (this.formElementsCount < 3) {
-      return 2000
-    }  else if (this.formElementsCount < 6) {
-      return 3000
-    }
-
-    return 5500
-  }
-},
-
 methods: {
   onSubmit: function () {
-    if(this.isSuspiciousUser || new Date() - this.time < this.acceptableTime) {
-      this.isCaptchaShown = true
-      setTimeout(() => {
-        this.getCaptcha.validate();
-      }, 500)
-    } else {
-      this.onVerify();
-      this.formTime = null;
-    }
+    console.log('Submitting the invisible hCaptcha', this.$refs.invisibleHcaptcha);
+    this.$refs.invisibleHcaptcha.execute();
   },
 
-  onVerify: async function () {
+  onVerify: async function (token, ekey) {
     var 
       response = '',
       data = document.querySelectorAll('[data-gsp-data]');
@@ -148,104 +74,22 @@ methods: {
       response += item.dataset.gspName + '=' + encodeURIComponent(item.dataset.gspData)
     });
 
+    this.verified = true;
+    console.log(token)
     await this.$gspPostForm(this.gscriptID, response)
   },
 
+
   onError: function () {
     console.log('captcha error')
+    this.token = null;
+    this.eKey = null;
     this.isError = true;
-  },
-
-  checkUserType: function () {
-    this.isSuspiciousUser = true;
-  },
-
-  checkCaptcha: function () {
-    this.isError = false
-  },
-
-  startInteractingWithForm() {
-    if(!this.time) {
-      this.time = new Date();
-    }
-  },
-
-  detectFormInteraction(e, elements) {
-    if(this.$refs.form && elements) {
-      elements.forEach(el => {
-        if(e.target === el) {
-          this.formElementsCount = this.formElementsCount + 1;
-          this.startInteractingWithForm();
-        }
-      })
-    }
   }
 },
-
-mounted() {
-
-  this.allInputs =  this.$refs.form.querySelectorAll('input');
-  this.allTextareas =  this.$refs.form.querySelectorAll('textarea');
-  
-  window.addEventListener('click', (e) => {
-    this.detectFormInteraction(e, this.allInputs);
-    this.detectFormInteraction(e, this.allTextareas);
-  })
-
-},
-
-beforeDestroy() {
-  this.time = null;
-  this.formElementsCount = 0;
-  window.removeEventListener('click', (e) => {
-    this.detectFormInteraction(e, this.allInputs);
-    this.detectFormInteraction(e, this.allTextareas);
-  })
-}
 
 }
 
 </script>
 
-<style>
-
-
-/* honeypot */
-label[for="user-type"], #user-type {
-  display: none;
-  visibility: hidden;
-}
-/* honeypot */
-
-form {
-  position: relative;
-}
-
-.jCaptchaCanvas {
-  position: absolute;
-  top: -25px;
-  left: 15px;
-}
-
-.captcha-error button {
-  background-color: rgb(181, 3, 0) !important;
-  color: #fff !important;
-}
-
-/* too many tries error */
-div.noForm {
-  font-size: 20px;
-  padding: 20px;
-  color: #fff;
-  font-weight: 600;
-  font-style: italic;
-  background-color: rgba(184, 0, 0, 0.753);
-  border: 3px double rgba(24, 24, 24, 0.772);
-}
-
-.noForm p {
-  margin: 0;
-}
-
-
-</style>
+<style></style>
